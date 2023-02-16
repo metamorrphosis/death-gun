c61a861489a83def97bdb52a10e05f66db55974d
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-02-17 00:51:05 +0100

    Update unit-tests.py

--- unit-tests.py
+++ unit-tests.py
@@ -10,7 +10,7 @@ load_dotenv()
 main_bot = MainBot()
 
 async def bot_start():
-    main_bot.load_extensions()
+    # main_bot.load_extensions()
     main_bot.run(os.getenv('BOT_TOKEN'))
     await main_bot.wait_until_ready()
     main_bot.close()
