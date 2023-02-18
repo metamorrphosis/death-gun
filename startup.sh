6d23adf1ba8c40c0ef875fabe5b54ecda62da554
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-02-18 05:17:14 +0100

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-git --version
-git config --global user.name "metamorrphosis"
-git config --global user.password "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"
-git init
-git pull https://github.com/metamorrphosis/death-gun.git main
-git --version
