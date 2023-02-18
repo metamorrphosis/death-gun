2b752e632988ca31576c95d90452006716a83b4b
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-02-18 04:31:22 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
+git --version
 git config --global user.name "metamorrphosis"
-git config --global user.password "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"
-cd death-gun
-git branch --set-upstream-to=origin/main main
-git fetch
-git pull
-python bot.py
+git config --global user.password "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"
+git add *
+git stash
+git pull https://github.com/metamorrphosis/death-gun.git main
+git stash apply --index
