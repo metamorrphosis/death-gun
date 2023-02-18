dd9f5b72185bb1963565205b99c51acbffd1d21a
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-02-18 04:41:39 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,5 @@ git --version
 git config --global user.name "metamorrphosis"
 git config --global user.password "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"
 git init
-git add .
-git stash -a
 git pull https://github.com/metamorrphosis/death-gun.git main
-git stash apply --index
+git --version
