cfc36e989dcd2d19a0f0558b097d084885eef528
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-02-19 03:49:57 +0100

    Update info_select_menu.py

--- cogs/other/info_select_menu.py
+++ cogs/other/info_select_menu.py
@@ -3,6 +3,8 @@ import json
 import discord 
 from discord.ext import commands
 
+picturue_embed = ''
+text_embed = {'image': {'url': 'https://cdn.discordapp.com/attachments/1053963528735838220/1076218146505101492/1676660790819.png', 'proxy_url': 'https://media.discordapp.net/attachments/1053963528735838220/1076218146505101492/1676660790819.png', 'width': 756, 'height': 3}, 'fields': [], 'color': 15645576, 'type': 'rich', 'description': '**<:tochkaicon:1075458720659689533> Медиа** - информация о социальных сетях ДезГана\n**<:tochkaicon:1075458720659689533> Роли** - информация об основных ролях сервера\n**<:tochkaicon:1075458720659689533> Развлечения** - информация о развлечениях сервера и как ими пользоваться', 'title': 'Выбери нужный тебе раздел'}
 class InfoSelectMenuCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -11,14 +13,9 @@ class InfoSelectMenuCog(commands.Cog):
     @commands.guild_only()
     @commands.has_guild_permissions( administrator = True )
     async def info_menu(self, ctx):
-        with open('resources/embeds/info_main.json') as embed_file:
-            embed_json = embed_file.read()
-        
-        parsed_json = json.loads(embed_json)
-        print(parsed_json)
-        print(type(parsed_json))
-        embed = discord.Embed().from_dict({'image': {'url': 'https://cdn.discordapp.com/attachments/1053963528735838220/1076218146505101492/1676660790819.png', 'proxy_url': 'https://media.discordapp.net/attachments/1053963528735838220/1076218146505101492/1676660790819.png', 'width': 756, 'height': 3}, 'fields': [], 'color': 15645576, 'type': 'rich', 'description': '**<:tochkaicon:1075458720659689533> Медиа** - информация о социальных сетях ДезГана\n**<:tochkaicon:1075458720659689533> Роли** - информация об основных ролях сервера\n**<:tochkaicon:1075458720659689533> Развлечения** - информация о развлечениях сервера и как ими пользоваться', 'title': 'Выбери нужный тебе раздел'})
-        
+        embeds = [
+            discord.Embed().from_dict(text_embed)
+        ]
         await ctx.message.delete()
         await ctx.send(embed = embed)
 
