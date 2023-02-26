54dd8236009ef213e4aa30604aa9addbe1d82a94
metamorrphosis <75tima75@gmail.com>
2023-02-26 12:46:09 +0100

    edit

--- cogs/economy/core.py
+++ cogs/economy/core.py
@@ -31,63 +31,42 @@ class EconomyCog(commands.Cog):
                 )
             ]
         )
-    '''
+    
     @commands.command(aliases = ['add-money', 'am', 'выдать-деньги', 'выдатьденьги', 'аддмоней', 'монейадд'])
     @commands.guild_only()
-    async def addmoney(self, ctx, member: Union[discord.Member, str] = None, mode = None, value = None):
+    async def addmoney(self, ctx, member: Union[discord.Member, str] = None, value = None):
         usage_field = discord.EmbedField(
             name = 'Использование команды',
-            value = f'`{prf}add-money <ник, упоминание или ID участника> <куда выдать (наличные, банк), если указать тут не режим а число — выдача в банк> [количество, указывать только если указан режим]`',
-        )
-        examples_field = discord.EmbedField(
-            name = 'Примеры использования команды',
-            value = f'`{prf}add-money @Петя228 5000` — поскольку вместо режима число — выдаст пете 5000 валюты в банк\
-            \n\n`{prf}add-money 1007615585506566205 cash 10,000` — выдаст участнику по ID 10,000 валюты в наличные'
+            value = f'`{_prefix}add-money <ник, упоминание или ID участника> <количество>`',
         )
 
         if member is None:
-            return await ctx.error(description = 'Вы не указали участника, которому необходимо выдать валюту в первом аргументе', fields = [usage_field, examples_field])
+            return await ctx.error_reply(description = 'Вы не указали участника, которому необходимо выдать валюту', fields = [usage_field])
         
         if not(isinstance(member, discord.Member)):
-            return await ctx.error(description = 'Участник не найден')
+            return await ctx.error_reply(description = 'Участник не найден')
 
-        if mode is None:
-            return await ctx.error(description = 'Вы не указали режим, куда нужно выдать деньги, либо число в втором аргументе', fields = [usage_field, examples_field])
-        
-        cash_list = ['cash', 'наличные', 'кеш', 'наличка']
-        bank_list = ['bank', 'банк']
-
-        if mode in cash_list:
-            mode = 'cash'
-        elif mode in bank_list:
-            mode = 'bank'
-        elif mode.isdigit():
-            value = mode
-            mode = 'bank'
-        else:
-            return await ctx.error(description = 'Вы не указали не режим и не положительное число вторым аргументом', fields = [usage_field, examples_field])
-        
         if value is None:
-            return await ctx.error(description = 'Вы указали режим, и не указали положительное число в третьем аргументе', fields = [usage_field, examples_field])
+            return await ctx.error_reply(description = 'Вы не указали количество валюты', fields = [usage_field])
 
         if not(value.isdigit()):
-            return await ctx.error(description = 'Вы указали не положительное число в третьем аргументе', fields = [usage_field, examples_field])
+            return await ctx.error_reply(description = 'Вы указали не правильное количество валюты. Оно должно быть положительным', fields = [usage_field])
         
         value = int(value)
 
-        if value >= 1000000000000000000:
-            return await ctx.error(description = f'Число не может быть больше {nc("1000000000000000000")}')
+        if value >= 100_000_000_000_000_000:
+            return await ctx.error_reply(description = f'Число не может быть больше 100,000,000,000,000,000 {_currency}')
 
         await self.db.add_money(
             member = member,
-            mode = mode,
             value = value
         )
 
-        await ctx.success(
-            description = f'Выдал {nc(str(value))}<:vajno_2:1018512718585679882> {member.mention} (`{member}`) в {"банк" if mode == "bank" else "наличные"}'
+        await ctx.success_reply(
+            description = f'Выдал {value:,} {_currency} {member.mention} (`{member}`)'
         )
     
+    '''
     @commands.command(aliases = ['reset-money', 'rm', 'сброс-денег', 'сбросденьги', 'ресетмоней', 'ресет-моней'])
     @commands.guild_only()
     @commands.has_guild_permissions( administrator = True )
