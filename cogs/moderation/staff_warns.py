1fcf0b5dfa5eb6e1c5e1a5498a28d2a2988a9623
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-02-28 15:30:14 +0200

    add

---
+++ cogs/moderation/staff_warns.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+from utils import staff_warns_db
+class StaffWarnsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = staff_warns_db.StaffWarnsDB()
+    
+    @commands.command(aliases = ['выговоры'])
+    @commands.guild_only()
+    async def staff_warns_command(self, ctx, _member: discord.Member = None):
+        member = _member or ctx.author
+
+        warns = await self.db.get_warns(member = member)
+
+        if len(warns) == 0:
+            return await ctx.neutral_reply(
+                title = f'Выговоры {member}'
+                descripiton = 'У данного участника отсутствуют выговоры'
+            )
+        
+
+
+def setup(bot):
+    bot.add_cog()
---
+++ utils/staff_warns_db.py
@@ -0,0 +1,35 @@
+import asyncio
+from datetime import datetime, timedelta
+
+import discord
+from motor.motor_asyncio import AsyncIOMotorClient
+
+class StaffWarnsDB:
+    def __init__(self):
+        self.cluster = AsyncIOMotorClient(os.getenv("MONGODB_LINK"))
+        self.warns = self.cluster["warns"]["staff_warns"]
+    
+    async def insert_warn(self, *, _id, author, member, reason):
+        if await self.warns.find_one({"_id": _id}) is None:
+            new_warn = {}
+            new_warn["_id"] = _id
+            new_warn["author"] = author.id
+            new_warn["member"] = member.id
+            new_warn["reason"] = reason
+            await self.warns.insert_one(new_warn)
+            return True
+        else:
+            return False
+
+    async def remove_warn(self, *, _id):
+        if await self.warns.find_one({"_id": _id}) is not None:
+            await self.warns.delete_one({"_id":_id})
+            return True
+        else:
+            return False
+
+    async def get_warns(self, *, member):
+        result = []
+        async for i in self.warns.find():
+            if i["member"] == member.id:
+                result.append(i)
