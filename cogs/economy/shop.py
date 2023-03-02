a3583d4b47e2697fee738de770faa4fd12263d6e
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-03-02 22:30:25 +0200

    add

---
+++ cogs/economy/shop.py
@@ -0,0 +1,46 @@
+import os
+import aiohttp
+
+import discord
+from discord.ext import commands
+
+from utils import economy_db
+from utils import staff_roles as staff_roles_util
+
+_prefix = os.getenv('BOT_PREFIX')
+_currency = os.getenv('CURRENCY')
+
+
+class ShopCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = economy_db.EconomyDB()
+    
+    @commands.command(aliases = ['store', 'магазин'])
+    @commands.guild_only()
+    async def shop(self, ctx):
+        pass
+        
+        '''
+        TODO:
+
+        ЧЕГО НЕТ В ШОПЕ:
+        Личная роль - 99 дон.
+        Войс комната - 249 дон.
+        В стафф без набора - 349 дон.
+        Гильдия - 149 дон.
+
+        ПРИВИЛЕГИИ:
+        Prime - 89 дон./мес.
+        Fabulous - 169 дон./мес
+        Blessed - 219 дон./мес
+        Absolute - 299 дон./мес
+        God - 399 дон./мес
+
+        КАЗИНО:
+        500 лепестков (валюта анбеливы) будет равняться одному донат коину (валюта нашего кастом бота)
+        все циферки выше сказал скай
+        '''
+
+def setup(bot):
+    bot.add_cog(ShopCog(bot))
