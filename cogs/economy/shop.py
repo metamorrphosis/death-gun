76f0d00d01303e00c18c012054dcb1dcff5eaee0
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2023-03-14 16:56:09 +0200

    fix

--- cogs/economy/shop.py
+++ cogs/economy/shop.py
@@ -110,7 +110,7 @@ class ShopSelectMenuView(discord.ui.View):
         custom_id = 'shop_select_menu',
         options = [
             discord.SelectOption(
-                value = 'casino',
+                value = 'voice',
                 label = 'Личный голосовой канал',
                 description = 'Ваш личный голосовой канал, доступ к которому есть у вас и ваших друзей',
                 emoji = discord.PartialEmoji.from_str('<:tochkaicon:1075458720659689533>')            
@@ -137,8 +137,21 @@ class ShopSelectMenuView(discord.ui.View):
         ]
     )
     async def select_callback(self, select, interaction):
-        if select.values[0] == 'casino':
-            await interaction.response.send_modal(CasinoModal())
+        if select.values[0] == 'voice':
+            member_bal = await self.db.get_money(member = interaction.user)
+            member_bal = member_bal["bal"]
+
+            if member_bal < 149:
+                return await interaction.response.send_message(f'У вас нету столько донаткоинов\n' \
+                                                        f'Ваш текущий баланс: **{member_bal}{_currency}**\n' \
+                                                        f'Голосовой канал стоит: **149{_currency}**\n',
+                                                        ephemeral = True)
+            
+            await self.db.remove_money(member = interaction.user, value = 149)
+            await interaction.response.send_message(f'ID покупки: `{shortuuid.ShortUUID().random(length=22)}`\n' \
+                                                    f'Вы успешно приобрели голосовой канал\n' \
+                                                    f'Для получения голосового канала откройте тикет и отправьте скриншот с этим сообщением', 
+                                                    ephemeral = True)
         
         elif select.values[0] == 'guild':
             member_bal = await self.db.get_money(member = interaction.user)
@@ -171,6 +184,10 @@ class ShopSelectMenuView(discord.ui.View):
                                                     f'Вы успешно приобрели саппорта\n' \
                                                     f'Для получения роли откройте тикет и отправьте скриншот с этим сообщением', 
                                                     ephemeral = True)
+        
+        elif select.values[0] == 'casino':
+            await interaction.response.send_modal(CasinoModal())
+        
 
 
 class CasinoModal(discord.ui.Modal):
